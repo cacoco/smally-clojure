6e3e68449ff840f6b412615a8a990dedb8109b4a
Christopher Coco <cacoco@gmail.com>
2011-10-20 16:58:55 -0700

    Rolled back to clojure 1.2.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject smally-clojure "0.1.0-SNAPSHOT"
             :description "A simple url shortener written in clojure+redis using the noir framework."
-            :dependencies [[org.clojure/clojure "1.3.0"]
+            :dependencies [[org.clojure/clojure "1.2.1"]
                            [clj-redis "0.0.12"]
                            [noir "1.2.0"]]
             :main smally-clojure.server)
